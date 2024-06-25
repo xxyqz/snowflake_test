84f57b810d093d9de7b9fadad363c0e516bfebdd
xxyqz <luyan6339@gmail.com>
2024-06-25 15:35:16 +0800

    update yml and ddl

--- snowsql/ddl/create_schema.sql
+++ snowsql/ddl/create_schema.sql
@@ -1,4 +1,6 @@
  use role sysadmin;
  use warehouse COMPUTE_WH;
  use database YL_DB;
- create or replace schema YL_DB.DCC_TEST;
+ create or replace schema DCC_TEST;
+ use schema DCC_TEST;
+ create or replace table t_test1(id number,name text);
